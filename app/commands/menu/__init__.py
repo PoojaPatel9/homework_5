8eb79ec07374b99ccc76b3e4fdf55977620d12ec
PoojaPatel9 <prp25@njit.edu>
2025-02-23 00:30:44 -0500

    menu __init__.py file updated and menu setup done

--- app/commands/menu/__init__.py
+++ app/commands/menu/__init__.py
@@ -4,5 +4,4 @@ from app.commands import Command
 
 class MenuCommand(Command):
     def execute(self):
-        print(f'Menu')
-        
+        print(f'Menu\n \n1. greet\n2. goodbye\n3. discord\n4. calculator\n5. exit\n ')
