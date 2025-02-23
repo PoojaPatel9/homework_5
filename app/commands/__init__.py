71b3d6a93c959e3620547df5ec894dc001186676
PoojaPatel9 <prp25@njit.edu>
2025-02-23 00:31:34 -0500

    command __init__py file updated to implement menu

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -12,15 +12,10 @@ class CommandHandler:
     def register_command(self, command_name: str, command: Command):
         self.commands[command_name] = command
 
-    def execute_command(self, command_name: str):
-        """ Look before you leap (LBYL) - Use when its less likely to work
-        if command_name in self.commands:
-            self.commands[command_name].execute()
-        else:
-            print(f"No such command: {command_name}")
-        """
-        """Easier to ask for forgiveness than permission (EAFP) - Use when its going to most likely work"""
+    def execute_command(self, command_name: str, *args):
+        """Easier to ask for forgiveness than permission (EAFP) - Use when it's going to most likely work"""
         try:
-            self.commands[command_name].execute()
+            return self.commands[command_name].execute(*args)
         except KeyError:
             print(f"No such command: {command_name}")
+            
