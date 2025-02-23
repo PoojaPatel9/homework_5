5ffab361fc62511b2aa4612506cfbc0084bc76ff
PoojaPatel9 <prp25@njit.edu>
2025-02-23 00:29:38 -0500

    calculator __init__.py file updated

--- app/commands/calculator/__init__.py
+++ app/commands/calculator/__init__.py
@@ -2,38 +2,24 @@ import sys
 from app.commands import Command
 
 # Command Implementations
-class AddCommand(Command):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+# app/commands/add_command.py
+class AddCommand:
+    def execute(self, a, b):
+        return a + b
+    
+# app/commands/add_command.py
+class SubtractCommand:
+    def execute(self, a, b):
+        return a - b
 
-    def execute(self):
-        print(f"Result: {self.a + self.b}")
 
-class SubtractCommand(Command):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+class MultiplyCommand:
+    def execute(self,a,b):
+        return a * b
 
-    def execute(self):
-        print(f"Result: {self.a - self.b}")
+class DivideCommand:
+    def execute(self,a,b):
+        if b == 0:
+            return "Error: Division by zero"
+        return a / b
 
-class MultiplyCommand(Command):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def execute(self):
-        print(f"Result: {self.a * self.b}")
-
-class DivideCommand(Command):
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
-
-    def execute(self):
-        if self.b == 0:
-            print("Error: Division by zero")
-        else:
-            print(f"Result: {self.a / self.b}")
-            
