c0b51b317e227859485afebde22d7d30741a7ad1
PoojaPatel9 <prp25@njit.edu>
2025-02-24 23:08:00 -0500

    Menu command implemented and test pass with 95% test coverage

--- app/commands/menu/__init__.py
+++ app/commands/menu/__init__.py
@@ -1,8 +1,29 @@
 import sys
 from app.commands import Command
+from app.commands.discord import DiscordCommand
+from app.commands.exit import ExitCommand
+from app.commands.goodbye import GoodbyeCommand
+from app.commands.greet import GreetCommand
+from app.calculator import main as calculator_main
 
 
 class MenuCommand(Command):
+    def __init__(self):
+        self.commands = {}
+        self._build_commands()
     def execute(self):
-        print(f'Menu\n \n1. greet\n2. goodbye\n3. discord\n4. calculator\n5. exit\n ')
+        print("Available commands:")
+        for command in self.commands:
+            print(f"- {command}")
+        print("- calculator")
+    
+    def _build_commands(self):
+        self.commands = {
+            'greet': GreetCommand(),
+            'goodbye':GoodbyeCommand(),
+            'discord':DiscordCommand(),
+            'exit':ExitCommand(),
+            'menu': self.execute
+        }
+
     
