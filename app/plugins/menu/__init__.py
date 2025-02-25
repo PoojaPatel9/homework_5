2c6900072c336fb31572950d391f5fc78d22eef8
PoojaPatel9 <prp25@njit.edu>
2025-02-24 21:41:46 -0500

    file updated and get 92% test coverage

--- app/plugins/menu/__init__.py
+++ app/plugins/menu/__init__.py
@@ -4,5 +4,5 @@ from app.commands import Command
 
 class MenuCommand(Command):
     def execute(self):
-        print(f'Menu\n \n1. greet\n2. goodbye\n3. discord\n4. calculator\n5. email\n\n \nCALCULATOR\n6. add\n7. subtract\n8. multiply\n9. divide\n10. exit ')
+        print(f'Menu\n \n1. greet\n2. goodbye\n3. discord\n4. email\n\n \nCALCULATOR\n5. add\n6. subtract\n7. multiply\n8. divide\n9. exit ')
     
